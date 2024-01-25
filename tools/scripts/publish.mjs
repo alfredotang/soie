8aad2c76e5e82106a6fe46bd6938256a593d2f85
Alfredo Tang <asdzxcca0617@gmail.com>
2024-01-25 23:37:02 +0800

    chore(@soie/fetcher): fix release tag

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -24,8 +24,7 @@ function invariant(condition, message) {
 
 // Executing publish script: node path/to/publish.mjs {name} --version {version} --tag {tag}
 // Default "tag" to "next" so we won't publish the "latest" tag by accident.
-const [, , name] = process.argv
-const tag = 'next'
+const [, , name, tag = 'latest'] = process.argv
 const graph = readCachedProjectGraph()
 const project = graph.nodes[name]
 
