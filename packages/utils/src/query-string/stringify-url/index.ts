5945e1293a86d466470a63d6beee1e3424801da5
Alfredo Tang <asdzxcca0617@gmail.com>
2024-04-06 20:23:00 +0800

    refactor(@soie/utils/query-string): mv options to second args

--- packages/utils/src/query-string/stringify-url/index.ts
+++ packages/utils/src/query-string/stringify-url/index.ts
@@ -2,14 +2,14 @@ import stringify from '@soie/utils/query-string/stringify'
 import type { StringifyOptions } from '@soie/utils/query-string/types'
 import type { StringifiableRecord } from '@soie/utils/types'
 
-export default function stringifyUrl({
-  url,
-  query,
-  options,
-}: {
+type StringifyUrlParams = {
   url: string
   query?: StringifiableRecord
+}
+
+export default function stringifyUrl(
+  { url, query }: StringifyUrlParams,
   options?: StringifyOptions
-}) {
+) {
   return [url, stringify(query, options)].filter(Boolean).join('?')
 }
